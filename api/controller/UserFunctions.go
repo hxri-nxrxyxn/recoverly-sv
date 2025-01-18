583c0b6d7ac5613483aa0cbd921e80382ce9fc5d
Sebastian <sebastianabraham2006@gmail.com>
2025-01-18 16:13:56 +0530

    added get user by id and its routes

--- api/controller/UserFunctions.go
+++ api/controller/UserFunctions.go
@@ -9,6 +9,32 @@ import (
 	"gorm.io/gorm"
 )
 
+func GetUser(db *gorm.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		user := new(models.User)
+		err := db.Where("UserID = ?", id).First(user).Error
+		if err != nil {
+			if strings.Contains(err.Error(), "record not found") {
+				return c.Status(404).JSON(fiber.Map{
+					"message": "User not found",
+				})
+			}
+
+			return c.Status(500).JSON(fiber.Map{
+				"message": "Could not retrieve user",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"message": "Retrieved user",
+			"data":    user,
+		})
+	}
+}
+
 func CreateUser(db *gorm.DB) func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		user := new(models.User)
