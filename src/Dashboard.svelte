cc6b1c81b8b937d1c6a1600eb66fa1509d176b0a
Hari Narayan <harinarayanmr@aol.com>
2025-01-19 04:28:35 +0530

    fixed merge conflicts

--- src/Dashboard.svelte
+++ src/Dashboard.svelte
@@ -1,105 +1,101 @@
 <script>
-<<<<<<< HEAD
     import { Link, useRouter } from "svelte-routing";
     import Morning from "./assets/undraw_mornings_kmib.png";
     import { Storage } from "@capacitor/storage";
     import Trophy from "./assets/trophy-star-2.svg";
-=======
-  import { Link, useRouter } from "svelte-routing";
-  import Morning from "./assets/undraw_mornings_kmib.png";
-  import { Storage } from "@capacitor/storage";
->>>>>>> 6db61c96feca578b696455888ab5691b8930b881
 
-  // navigation
+    // navigation
 
-  const router = useRouter();
-  let hasToken = true; // Start as null to represent "loading"
+    const router = useRouter();
+    let hasToken = true; // Start as null to represent "loading"
 
-  let claims;
+    let claims;
 
-  const logout = async () => {
-    try {
-      await Storage.remove({ key: "id" });
-      location.href = "/login";
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+    const logout = async () => {
+        try {
+            await Storage.remove({ key: "id" });
+            location.href = "/login";
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
 
-  const checkUser = async () => {
-    try {
-      const value = await Storage.get({ key: "id" });
-      const id = value.value;
-      if (!id) {
-        hasToken = false;
-        location.href = "/login";
-      }
-      const response = await fetch(`http://localhost:8080/api/v1/users/${id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+    const checkUser = async () => {
+        try {
+            const value = await Storage.get({ key: "id" });
+            const id = value.value;
+            if (!id) {
+                hasToken = false;
+                location.href = "/login";
+            }
+            const response = await fetch(
+                `http://localhost:8080/api/v1/users/${id}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                },
+            );
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
 
-  checkUser();
+    checkUser();
 </script>
 
 {#if hasToken === true}
-  <main>
-    <div class="pills">
-      <div class="pill">
-        <p>LEVEL</p>
-      </div>
-      <div class="pill">
-        <p>EVENTS</p>
-      </div>
-      <div class="pill">
-        <p>CHATROOM</p>
-      </div>
-    </div>
-    <h1>Good morning, <span>Hari</span></h1>
-
-    <img src={Morning} alt="" class="hero" />
-
-    <div class="card">
-      <h1>Daily Task</h1>
-      <div class="tasks">
-        <div class="task">
-          <div class="task__text">
-            <h5>No. of minutes meditated</h5>
-            <p>0/15</p>
-          </div>
-          <div class="task__adjust">
-            <button>-</button>
-            <h5>0</h5>
-            <button>+</button>
-          </div>
+    <main>
+        <div class="pills">
+            <div class="pill">
+                <p>LEVEL</p>
+            </div>
+            <div class="pill">
+                <p>EVENTS</p>
+            </div>
+            <div class="pill">
+                <p>CHATROOM</p>
+            </div>
         </div>
-        <div class="task">
-          <div class="task__text">
-            <h5>No. of cigarretes smoked</h5>
-            <p>0/7</p>
-          </div>
-          <div class="task__adjust">
-            <button>-</button>
-            <h5>0</h5>
-            <button>+</button>
-          </div>
-        </div>
-        <button onclick={logout}>submit</button>
-      </div>
-    </div>
+        <h1>Good morning, <span>Hari</span></h1>
 
-<<<<<<< HEAD
+        <img src={Morning} alt="" class="hero" />
+
+        <div class="card">
+            <h1>Daily Task</h1>
+            <div class="tasks">
+                <div class="task">
+                    <div class="task__text">
+                        <h5>No. of minutes meditated</h5>
+                        <p>0/15</p>
+                    </div>
+                    <div class="task__adjust">
+                        <button>-</button>
+                        <h5>0</h5>
+                        <button>+</button>
+                    </div>
+                </div>
+                <div class="task">
+                    <div class="task__text">
+                        <h5>No. of cigarretes smoked</h5>
+                        <p>0/7</p>
+                    </div>
+                    <div class="task__adjust">
+                        <button>-</button>
+                        <h5>0</h5>
+                        <button>+</button>
+                    </div>
+                </div>
+                <button onclick={logout}>submit</button>
+            </div>
+        </div>
+
         <div class="achievements">
             <h2>Achivements</h2>
             <div class="achievement">
@@ -118,40 +114,27 @@
                     <p>more awards</p>
                 </div>
             </div>
-=======
-    <div class="achievements">
-      <h2>Achivements</h2>
-      <div class="achievement">
-        <div class="achievement__logo">
-          <i>logo</i>
->>>>>>> 6db61c96feca578b696455888ab5691b8930b881
         </div>
-        <div class="achievement__text">
-          <p>Fresh Start</p>
+        <div class="achievement">
+            <div class="achievement__logo">
+                <i>logo</i>
+            </div>
+            <div class="achievement__text">
+                <p>Fresh Start</p>
+            </div>
         </div>
-      </div>
-      <div class="achievement">
-        <div class="achievement__logo">
-          <i>logo</i>
-        </div>
-        <div class="achievement__text">
-          <p>Fresh Start</p>
-        </div>
-      </div>
-    </div>
 
-    <div class="emergency">
-      <Link to="/emergency">SOS</Link>
-    </div>
-  </main>
+        <div class="emergency">
+            <Link to="/emergency">SOS</Link>
+        </div>
+    </main>
 {:else if hasToken === false}
-  <p>Redirecting to login...</p>
+    <p>Redirecting to login...</p>
 {:else}
-  <p>Loading...</p>
+    <p>Loading...</p>
 {/if}
 
 <style>
-<<<<<<< HEAD
     /* Your styles */
     .task {
         display: flex;
@@ -211,55 +194,4 @@
     .achievement__logo img {
         transform: translateY(0.2rem);
     }
-=======
-  /* Your styles */
-  .task {
-    display: flex;
-    justify-content: space-between;
-    margin-top: 1rem;
-  }
-  .task__adjust {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  .task__adjust > * {
-    margin-left: 1rem;
-  }
-  .task__adjust button {
-    position: initial;
-    font-size: 1rem;
-    padding: 0.5rem;
-    border: 0;
-    color: var(--color-text);
-    background-color: var(--color-light);
-  }
-  .task__adjust h5 {
-    text-align: center;
-  }
-  .task__text p {
-    font-size: 0.75rem;
-  }
-  .pills {
-    display: flex;
-    margin-bottom: 1rem;
-  }
-  .pill {
-    background-color: var(--color-light);
-    padding: 0.25rem 1rem;
-    margin-right: 0.5rem;
-    border-radius: 2rem;
-  }
-  .pill p {
-    font-size: 0.5rem;
-    font-weight: 700;
-    color: var(--color-primary);
-  }
-  .achievements {
-    margin-top: 2rem;
-  }
-  .achievement {
-    display: flex;
-  }
->>>>>>> 6db61c96feca578b696455888ab5691b8930b881
 </style>
--- src/Login.svelte
+++ src/Login.svelte
@@ -5,44 +5,43 @@
     import { Storage } from "@capacitor/storage";
     import NavClassic from "./NavClassic.svelte";
 
-  let email = $state("");
-  let password = $state("");
+    let email = $state("");
+    let password = $state("");
 
-  const doPost = async () => {
-    const data = {
-      email: email,
-      password: password,
+    const doPost = async () => {
+        const data = {
+            email: email,
+            password: password,
+        };
+        const res = await fetch("http://localhost:8080/api/v1/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!res.ok) {
+            const { message } = await res.json();
+            alert(message);
+            return;
+        }
+
+        const { id } = await res.json();
+
+        const setToken = async () => {
+            await Storage.set({
+                key: "id",
+                value: id,
+            });
+            location.href = "/dashboard";
+        };
+
+        setToken();
     };
-    const res = await fetch("http://localhost:8080/api/v1/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (!res.ok) {
-      const { message } = await res.json();
-      alert(message);
-      return;
-    }
-
-    const { id } = await res.json();
-
-    const setToken = async () => {
-      await Storage.set({
-        key: "id",
-        value: id,
-      });
-      location.href = "/dashboard";
-    };
-
-    setToken();
-  };
 </script>
 
 <main>
-<<<<<<< HEAD
     <NavClassic />
     <h1>Let's beat our <span>addictions</span></h1>
     <img class="hero" src={Flower} alt="" />
@@ -65,36 +64,27 @@
                 bind:value={password}
             />
         </div>
-=======
-  <h1>Let's beat our <span>addictions</span></h1>
-  <img class="hero" src={Flower} alt="" />
-  <div class="form__box">
-    <div class="form__link">
-      <label>Email Id</label>
-      <input type="text" placeholder="Email" required bind:value={email} />
->>>>>>> 6db61c96feca578b696455888ab5691b8930b881
+        <div class="form__link">
+            <label>Password</label>
+            <input
+                type="password"
+                placeholder="Password"
+                required
+                bind:value={password}
+            />
+        </div>
     </div>
-    <div class="form__link">
-      <label>Password</label>
-      <input
-        type="password"
-        placeholder="Password"
-        required
-        bind:value={password}
-      />
-    </div>
-  </div>
 
-  <div class="btn__box">
-    <Link to="/org-login">
-      <button class="outlined">ORGANIZER LOGIN</button>
-    </Link>
-    <br />
-    <button class="solid" onclick={doPost}>LOGIN</button>
-    <p class="foot">
-      Don't have an account?<Link to="/register"><b>Create one</b></Link>
-    </p>
-  </div>
+    <div class="btn__box">
+        <Link to="/org-login">
+            <button class="outlined">ORGANIZER LOGIN</button>
+        </Link>
+        <br />
+        <button class="solid" onclick={doPost}>LOGIN</button>
+        <p class="foot">
+            Don't have an account?<Link to="/register"><b>Create one</b></Link>
+        </p>
+    </div>
 </main>
 
 <style>
