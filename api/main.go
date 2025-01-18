1a0d58bd5bd9453bc86764e06f5f10462b290125
Sebastian <sebastianabraham2006@gmail.com>
2025-01-18 12:21:53 +0530

    added database connection and migration of user to database

--- api/main.go
+++ api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/sebastian-abraham/recoverly/database"
 	"github.com/sebastian-abraham/recoverly/models"
 )
 
@@ -12,6 +15,11 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 	}))
+
+	db, err := database.NewConnection()
+	if err != nil {
+		log.Fatal("Could not load database")
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
