4cedf3d2dd8408f54085b5fbf0f7abb7263c8108
Hari Narayan <harinarayanmr@aol.com>
2025-01-18 11:56:35 +0530

    structure for login done

--- src/Login.svelte
+++ src/Login.svelte
@@ -1,2 +1,15 @@
-this is login
 <a href="/register">register</a>
+
+<main>
+    <h1>Let's beat our <span>addictions</span></h1>
+    <div class="form__box">
+        <div class="form__link">
+            <label>Email Id</label>
+            <input type="text" placeholder="Email" required />
+        </div>
+        <div class="form__link">
+            <label>Password</label>
+            <input type="password" placeholder="Password" required />
+        </div>
+    </div>
+</main>
