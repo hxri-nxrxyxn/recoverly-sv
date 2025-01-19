afebab7b7474780a50ba3933689ee559a7fdec45
Hari Narayan <harinarayanmr@aol.com>
2025-01-19 08:18:36 +0530

    navigation done

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -1,22 +1,38 @@
 <script>
     import Star from "./assets/noun-star-923969.svg";
     import ProfilePic from "./assets/circle-user.svg";
+    import { Link } from "svelte-routing";
 
-    const toggleView = () => {};
+    const viewDropDown = () => {
+        document.querySelector(".nav__dropdown").style.display = "block";
+    };
+    const hideDropDown = () => {
+        document.querySelector(".nav__dropdown").style.display = "none";
+    };
 </script>
 
 <main>
     <div class="nav">
         <div class="nav__icon">
-            <img src={Star} alt="" class="icon" />
+            <img src={Star} alt="" class="icon" onclick={viewDropDown} />
         </div>
         <div class="nav__dropdown">
-            <p>Home</p>
-            <p>Chatroom</p>
-            <p>Local Events</p>
-            <p>Templates & Guides</p>
-            <p>Service</p>
-            <p>AI Task Generation</p>
+            <p onclick={hideDropDown}>Home</p>
+            <Link to="/chatroom">
+                <p>Chatroom</p>
+            </Link>
+            <Link to="/events">
+                <p>Local Events</p>
+            </Link>
+            <Link>
+                <p>Templates & Guides</p>
+            </Link>
+            <Link>
+                <p>Service</p>
+            </Link>
+            <Link>
+                <p>AI Task Generation</p>
+            </Link>
         </div>
         <div class="nav__placeholder">hey</div>
         <div class="nav__stats">
