aea52929c85cc7aa64132726c71e14eb344ecbe9
Sebastian <sebastianabraham2006@gmail.com>
2025-01-19 13:44:04 +0530

    template implmented

--- api/models/Chat.go
+++ api/models/Chat.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Chat struct {
+	ChatID    uint      `gorm:"primaryKey;autoIncrement" json:"id"` // Chat ID
 	UserID    uint      `gorm:"not null" json:"user_id"`
 	Name      string    `gorm:"type:varchar(255)" json:"name"`       // User name
 	ChatGroup string    `gorm:"type:varchar(255)" json:"chat_group"` // Chat group name
--- src/Tasks.svelte
+++ src/Tasks.svelte
@@ -1,50 +1,50 @@
 <script>
-    import { Link } from "svelte-routing";
-    import { get } from "./fetch";
-    import { GoogleGenerativeAI } from "@google/generative-ai";
-    const API_KEY = "AIzaSyCjT3qZw8I6SYEqEH1x_681_4czeQB8OIw";
-    import Nav from "./Nav.svelte";
+  import { Link } from "svelte-routing";
+  import { get } from "./fetch";
+  import { GoogleGenerativeAI } from "@google/generative-ai";
+  const API_KEY = "AIzaSyCjT3qZw8I6SYEqEH1x_681_4czeQB8OIw";
+  import Nav from "./Nav.svelte";
 
-    const urlParams = new URLSearchParams(window.location.search);
-    let mytip = urlParams.get("tip"); // Get the value of 'category'
-    const generateAI = async (prompt) => {
-        const genAI = new GoogleGenerativeAI(API_KEY);
-        const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash",
-        });
-        try {
-            const result = await model.generateContent(prompt);
-            mytip = result.response.text();
-            console.log(mytip);
-        } catch (error) {}
-    };
+  const urlParams = new URLSearchParams(window.location.search);
+  let mytip = urlParams.get("tip"); // Get the value of 'category'
+  const generateAI = async (prompt) => {
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+    });
+    try {
+      const result = await model.generateContent(prompt);
+      mytip = result.response.text();
+      console.log(mytip);
+    } catch (error) {}
+  };
 
-    generateAI(
-        `dont't even have to include any backticks just return the data in a array format, as a series of tasks (max 3) with list of having whaat to do`,
-    );
+  generateAI(
+    `return in format of json {task : task, todo : ["sdf","sdfsad"]}, as a series of tasks (max 3) with list of having whaat to do`
+  );
 </script>
 
 <main>
-    <Nav />
-    <h1>Generate Effective <br /> <span>Goals</span></h1>
+  <Nav />
+  <h1>Generate Effective <br /> <span>Goals</span></h1>
 
-    <div class="tasks">
-        <div class="task">
-            <h3>Week 1</h3>
-            <p>Get started by</p>
-            <p class="task--sub">task 1</p>
-            <p class="task--sub">task 2</p>
-        </div>
+  <div class="tasks">
+    <div class="task">
+      <h3>Week 1</h3>
+      <p>Get started by</p>
+      <p class="task--sub">task 1</p>
+      <p class="task--sub">task 2</p>
     </div>
-    <div class="btn__box">
-        <Link to="/dashboard">
-            <button class="solid">CONTINUE</button>
-        </Link>
-    </div>
+  </div>
+  <div class="btn__box">
+    <Link to="/dashboard">
+      <button class="solid">CONTINUE</button>
+    </Link>
+  </div>
 </main>
 
 <style>
-    .progressi {
-        width: 30%;
-    }
+  .progressi {
+    width: 30%;
+  }
 </style>
